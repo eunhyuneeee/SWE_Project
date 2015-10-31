861bb0646e2cefb8dbd5c52c4c66d3e3c9b1da47
eunhyuneeee <eunhyun25@gmail.com>
2015-10-31 09:00:59 +0900

    DB클래스 분리

--- SWE_Project/src/StudentInfoDB.java
+++ SWE_Project/src/StudentInfoDB.java
@@ -1,30 +1,451 @@
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.*;
 
-public class StudentInfoDB extends StudentInfoSystem{
-	
-	String db_id ;
-	String db_name;
-	String db_depart;
-	String db_pnum ;
+public class StudentInfoDB {
 	
 	
-	public StudentInfoDB(String id, String name, String depart, String pnum){
-		db_id = id;
-		db_name = name;
-		db_depart = depart;
-		db_pnum = pnum;
-	}
+	Connection con = null;
+	java.sql.Statement st = null;
+	ResultSet rs = null;
+	int r = 0;
 	
-	// 학번이 동일한게 있거나 , 정보입력칸에 하나라도 입력하지 않는 경우 수행x 
-	public boolean info_add(){
-				
+	public void dataBase() {
+		String jdbcDriver = "com.mysql.jdbc.Driver";
+		String dbUrl = "jdbc:mysql://" + StudentInfoSystem._host + ":" + StudentInfoSystem._port + "/" + StudentInfoSystem._database;
+		String username = StudentInfoSystem._user;
+		String password = StudentInfoSystem._password;
+		
 		try {
-			state.executeUpdate("insert into student_info(Id, Name, Department, Phone_num) values('"+db_id+"','"+db_name+"','"+db_depart+"','"+db_pnum+"')");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			Class.forName(jdbcDriver).newInstance();
+			con = (Connection) DriverManager.getConnection(dbUrl,username,password);
+			st = con.createStatement();
+			
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		return true;	
 	}
+	
+	public void add(){				
+		
+		String id = StudentInfoSystem.input_id.getText().trim();	
+		try { 
+			String str = "select Id from student_info where Id LIKE '"+id+"' ";
+			rs = st.executeQuery(str);
+			
+			if(rs.next()) {
+				StudentInfoSystem.display.append("중복된 학번입니다. 다시 입력하세요.");
+			}		
+		} catch (SQLException e) {
+            e.printStackTrace();
+        }	
+		
+		String name = StudentInfoSystem.input_name.getText().trim();
+		String depart = StudentInfoSystem.input_depart.getText().trim();
+		String pnum = StudentInfoSystem.input_pnum.getText().trim();
+		
+		try {       
+            String sql2 = "insert into student_info values" + "('"+id+"','"+name+"','"+depart+"','"+pnum+"')";
+            st.executeUpdate(sql2); 
+            StudentInfoSystem.display.append("=============================================================" +"\n");
+            StudentInfoSystem.display.append(" \t     학생 정보가 추가되었습니다.\n ");
+            StudentInfoSystem.display.append(" ============================================================="+"\n");
 
-}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }	
+	}
+	
+	/*
+	public void dataSearch(){
+		System.out.print("책 제목을 입력해주세요: ");
+		String searchtitle = scan.nextLine();
+		
+		try {
+			String showData = " select * from book WHERE title LIKE '%"+searchtitle+"%' ";
+			rs = st.executeQuery(showData);
+			
+			if(rs.next()) {
+				rs=st.getResultSet();
+				
+				System.out.println("[search result]");
+				rs.beforeFirst();
+				
+				System.out.println("____________________________________________________________");
+				System.out.println(String.format("  %s  ", "book_number") + "|" 
+				                 + String.format(" %20s ", "title") + "|"  
+				                 + String.format(" %20s ", "publisher"));
+				System.out.println("____________________________________________________________");				
+				while(rs.next()) {					
+					System.out.println(String.format("     %5s     ", rs.getString(1)) + "|" 
+	                 		 + String.format(" %20s ",  rs.getString(2)) + "|"  
+	                         + String.format(" %20s ", rs.getString(3)));
+				}
+				System.out.println("____________________________________________________________");	
+				} else { 
+					System.out.print("검색 결과가 없습니다.If you want to go back to menu press1 => ");
+					int rg = scan.nextInt();
+					switch (rg) {
+					case 1:
+						mg.Manage();
+						break;
+					default:
+						System.out.println("Insert is wrong.");
+						break;
+					}
+					}
+			
+			System.out.println("");
+			
+			System.out.print("원하는책번호를입력하세요: ");
+			String searchbn = scan.nextLine();
+			
+			try {
+				String search = " select * from book WHERE book_number LIKE '"+searchbn+"' ";
+				rs=st.executeQuery(search);
+				
+				if(rs.next()) {
+					rs=st.getResultSet();
+					
+					System.out.println("[search result]");
+					rs.beforeFirst();
+					System.out.println("________________________________________________________________________________________________________________________");
+					System.out.println(String.format("  %s  ", "book_number") + "|" 
+					                 + String.format(" %20s ", "title") + "|"  
+					                 + String.format(" %20s ", "writer") + "|" 
+					                 + String.format(" %20s ", "publisher") + "|"
+					                 + String.format(" %10s ", "borrow 가능") + "|"
+					                 + String.format(" %20s ", "borrowed Id"));
+					System.out.println("________________________________________________________________________________________________________________________");
+					String can = "O";
+					while(rs.next()) {						
+						if(rs.getString(5)!=null) {
+							can = "X";
+						} 
+						
+						System.out.println(String.format("     %5s     ", rs.getString(1)) + "|" 
+		                 		 + String.format(" %20s ",  rs.getString(2)) + "|"  
+		                         + String.format(" %20s ", rs.getString(3)) + "|"  
+		                         + String.format(" %20s ", rs.getString(4)) + "|"
+		                         + String.format(" %10s ", can) + "|"
+		                         + String.format(" %20s ", rs.getString(5)));
+						}
+					System.out.println("________________________________________________________________________________________________________________________");
+					} else { 
+						System.out.print("잘못된 책 번호입니다. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+						int rg = scan.nextInt();
+						switch (rg) {
+						case 1:
+							mg.Manage();
+							break;
+						default:
+							System.out.println("Insert is wrong.");
+							break;
+						}
+						}
+				} catch(Exception e) {
+					System.out.println("Data가 없습니다.");
+					return;
+				}
+			} catch(Exception e) {
+				System.out.println("Data가 없습니다.");
+				return;
+			}
+	}
+	
+	public void dataDelete(){ 
+		System.out.print("삭제할 책 제목을 입력하세요: ");
+		String searchtitle = scan.nextLine();
+		
+		try {
+			String showData = " select * from book WHERE title LIKE '%"+searchtitle+"%' ";
+			rs = st.executeQuery(showData);
+			
+			if(rs.next()) {
+				rs=st.getResultSet();
+				
+				System.out.println("[search result]");
+				rs.beforeFirst();
+				System.out.println("________________________________________________________________________________________________________________________");
+				System.out.println(String.format("  %s  ", "book_number") + "|" 
+				                 + String.format(" %20s ", "title") + "|"  
+				                 + String.format(" %20s ", "writer") + "|" 
+				                 + String.format(" %20s ", "publisher") + "|"
+				                 + String.format(" %10s ", "borrow 가능") + "|"
+				                 + String.format(" %20s ", "borrowed Id"));
+				System.out.println("________________________________________________________________________________________________________________________");
+				String can = "O";
+				while(rs.next()) {						
+					if(rs.getString(5)!=null) {
+						can = "X";
+					} 
+					
+					System.out.println(String.format("     %5s     ", rs.getString(1)) + "|" 
+	                 		 + String.format(" %20s ",  rs.getString(2)) + "|"  
+	                         + String.format(" %20s ", rs.getString(3)) + "|"  
+	                         + String.format(" %20s ", rs.getString(4)) + "|"
+	                         + String.format(" %10s ", can) + "|"
+	                         + String.format(" %20s ", rs.getString(5)));
+					}
+				System.out.println("________________________________________________________________________________________________________________________");
+				} else { 
+					System.out.print("검색 결과가 없습니다. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+					int rg = scan.nextInt();
+					switch (rg) {
+					case 1:
+						mg.Manage();
+						break;
+					default:
+						System.out.println("Insert is wrong.");
+						break;
+					}
+					}
+			
+			System.out.println("");
+				
+			System.out.print("삭제할 책 번호를 입력하세요: ");
+			String delbook = scan.nextLine();
+			
+			try {
+				String delete = "delete from book where book_number = '"+delbook+"'";
+				r = st.executeUpdate(delete);
+				
+					
+				if(r!=0) { 
+					System.out.println("삭제 완료"); 
+					} else { 
+						System.out.print("삭제 실패. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+						int rg = scan.nextInt();
+						switch (rg) {
+						case 1:
+							mg.Manage();
+							break;
+						default:
+							System.out.println("Insert is wrong.");
+							break;
+						}
+						}
+				} catch(Exception e) {
+					System.out.println("Data가 없습니다.");
+					return;
+					}
+			} catch(Exception e) {
+				System.out.println("Data가 없습니다.");
+				return;
+				}
+		}
+
+	public void dataChange(){
+		System.out.print("업데이트하고 싶은 책 제목을 입력하세요 :");
+		String searchtitle = scan.nextLine();
+		
+		try {
+			String showData = " select * from book WHERE title LIKE '%"+searchtitle+"%' ";
+			rs = st.executeQuery(showData);
+			
+			if(rs.next()) {
+				rs=st.getResultSet();
+				
+				System.out.println("[search result]");
+				rs.beforeFirst();
+				System.out.println("________________________________________________________________________________________________________________________");
+				System.out.println(String.format("  %s  ", "book_number") + "|" 
+				                 + String.format(" %20s ", "title") + "|"  
+				                 + String.format(" %20s ", "writer") + "|" 
+				                 + String.format(" %20s ", "publisher") + "|"
+				                 + String.format(" %10s ", "borrow 가능") + "|"
+				                 + String.format(" %20s ", "borrowed Id"));
+				System.out.println("________________________________________________________________________________________________________________________");
+				String can = "O";
+				while(rs.next()) {						
+					if(rs.getString(5)!=null) {
+						can = "X";
+					} 
+					
+					System.out.println(String.format("     %5s     ", rs.getString(1)) + "|" 
+	                 		 + String.format(" %20s ",  rs.getString(2)) + "|"  
+	                         + String.format(" %20s ", rs.getString(3)) + "|"  
+	                         + String.format(" %20s ", rs.getString(4)) + "|"
+	                         + String.format(" %10s ", can) + "|"
+	                         + String.format(" %20s ", rs.getString(5)));
+					}
+				System.out.println("________________________________________________________________________________________________________________________");
+				} else { 
+					System.out.print("검색 결과가 없습니다. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+					int rg = scan.nextInt();
+					switch (rg) {
+					case 1:
+						mg.Manage();
+						break;
+					default:
+						System.out.println("Insert is wrong.");
+						break;
+					}
+					}
+			
+			System.out.println("");
+			
+			System.out.print("업데이트할 책 번호를 입력하세요: ");
+			String upbook = scan.nextLine();
+				
+			try
+			{
+				String showData4 = " select * from book WHERE book_number LIKE '"+upbook+"' ";
+				rs = st.executeQuery(showData4);
+				
+				if(rs.next()) {
+					rs=st.getResultSet();
+					
+					System.out.println("[update할 책 정보]");
+					rs.beforeFirst();
+					while(rs.next()) {
+						System.out.print(rs.getString(1)+" "); //System.out.print("책 번호 : ");
+						System.out.print(rs.getString(2)+" "); //System.out.print("책 제목 : ");
+						System.out.print(rs.getString(3)+" "); //System.out.print("저자 : ");
+						System.out.print(rs.getString(4)+" "); //System.out.print("출판사 : ");
+						System.out.println(rs.getString(5));   //System.out.print("대출 학생 ID : ");
+						}
+					
+					System.out.println("1.책 제목 변경 | 2.저자 변경 | 3.출판사 변경");
+					System.out.print("선택 : ");
+					int upnum = scan.nextInt();
+					scan.nextLine();
+					
+					if(upnum==1) { // 책 제목 변경 
+						
+						System.out.print("새로운 이름을 입력하세요: ");
+						String uptitle = scan.nextLine();
+						String updata = " update book set title='"+uptitle+"' where book_number LIKE '"+upbook+"' ";
+						st.executeUpdate(updata);
+						
+						if(st.executeUpdate(updata)>0) { 
+							System.out.println("업데이트 완료"); 
+							} else { 
+								System.out.print("업데이트 실패. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+								int rg = scan.nextInt();
+								switch (rg) {
+								case 1:
+									mg.Manage();
+									break;
+								default:
+									System.out.println("Insert is wrong.");
+									break;
+								}
+								}
+						
+						String showData3 = " select * from book WHERE book_number LIKE '"+upbook+"' ";
+						rs=st.executeQuery(showData3);
+						
+						if(rs!=null) {
+							rs=st.getResultSet();
+						}
+						
+						System.out.println("[update result]");
+						while(rs.next()) {
+							System.out.print(rs.getString(1)+" "); //System.out.print("책 번호 : ");
+							System.out.print(rs.getString(2)+" "); //System.out.print("책 제목 : ");
+							System.out.print(rs.getString(3)+" "); //System.out.print("저자 : ");
+							System.out.print(rs.getString(4)+" "); //System.out.print("출판사 : ");
+							System.out.println(rs.getString(5));   //System.out.print("대출 학생 ID : ");
+							}
+						} else if(upnum==2) { // 저자 변경
+							
+							System.out.print("새로운 저자를 입력하세요: ");
+							String upwriter = scan.nextLine();
+							String updata = " update book set writer='"+upwriter+"' where book_number LIKE '"+upbook+"' ";
+							st.executeUpdate(updata);
+							
+							if(st.executeUpdate(updata)>0) { 
+								System.out.println("업데이트 완료"); 
+								} else { 
+									System.out.print("업데이트 실패. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+									int rg = scan.nextInt();
+									switch (rg) {
+									case 1:
+										mg.Manage();
+										break;
+									default:
+										System.out.println("Insert is wrong.");
+										break;
+									}
+									}
+							
+							String showData3 = " select * from book WHERE book_number LIKE '"+upbook+"' ";
+							rs=st.executeQuery(showData3);
+							
+							if(rs!=null) {
+								rs=st.getResultSet();
+							}
+							
+							System.out.println("[update result]");
+							while(rs.next()) {
+								System.out.print(rs.getString(1)+" "); //System.out.print("책 번호 : ");
+								System.out.print(rs.getString(2)+" "); //System.out.print("책 제목 : ");
+								System.out.print(rs.getString(3)+" "); //System.out.print("저자 : ");
+								System.out.print(rs.getString(4)+" "); //System.out.print("출판사 : ");
+								System.out.println(rs.getString(5));   //System.out.print("대출 학생 ID : ");
+								}
+						} else if(upnum==3) {
+							
+							System.out.print("새로운 출판사를 입력하세요: ");
+							String uppublisher  = scan.nextLine();
+							String updata = " update book set publisher='"+uppublisher +"' where book_number LIKE '"+upbook+"' ";
+							st.executeUpdate(updata);
+							
+							if(st.executeUpdate(updata)>0) { 
+								System.out.println("업데이트 완료"); 
+								} else { 
+									System.out.print("업데이트 실패. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+									int rg = scan.nextInt();
+									switch (rg) {
+									case 1:
+										mg.Manage();
+										break;
+									default:
+										System.out.println("Insert is wrong.");
+										break;
+									}
+									}
+							
+							String showData3 = " select * from book WHERE book_number LIKE '"+upbook+"' ";
+							rs=st.executeQuery(showData3);
+							
+							if(rs!=null) {
+								rs=st.getResultSet();
+							}
+							
+							System.out.println("[update result]");
+							while(rs.next()) {
+								System.out.print(rs.getString(1)+" "); //System.out.print("책 번호 : ");
+								System.out.print(rs.getString(2)+" "); //System.out.print("책 제목 : ");
+								System.out.print(rs.getString(3)+" "); //System.out.print("저자 : ");
+								System.out.print(rs.getString(4)+" "); //System.out.print("출판사 : ");
+								System.out.println(rs.getString(5));   //System.out.print("대출 학생 ID : ");
+								}
+							}
+					} else { 
+						System.out.print("잘못된 책 번호입니다. 메뉴로 돌아가고 싶으면 1을 입력하세요 => ");
+						int rg = scan.nextInt();
+						switch (rg) {
+						case 1:
+							mg.Manage();
+							break;
+						default:
+							System.out.println("Insert is wrong.");
+							break;
+						}
+						}
+				} catch(SQLException e) {
+					System.out.println("Data가 없습니다.");
+					return;
+					}
+			} catch(Exception e) {
+				System.out.println("Data가 없습니다.");
+				return;
+				}
+		}
+
+ */
+	}
--- SWE_Project/src/StudentInfoSystem.java
+++ SWE_Project/src/StudentInfoSystem.java
@@ -1,121 +1,111 @@
-import java.sql.*;
-import java.awt.event.*;
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class StudentInfoSystem extends JFrame implements ActionListener{
+	static String _host = "localhost";
+	static String _port = "3306";
+	static String _user = "root";
+	static String _password = "0070";
+	static String _database = "student_info";
 	
-	private static final long serialVersionUID = 1L;
+	static JTextArea display;
+	static JTextField input_id, input_name, input_depart, input_pnum;
+	static JButton add, delete, update, view; 
 	
-	public static final int NONE = 0;
-	public static final int ADD = 1;
-	public static final int DELETE = 2;
-	public static final int UPDATE = 3;
-	public static final int VIEW = 4;
-	
-	public JTextArea display;
-	public JTextField jt_id, jt_name, jt_depart, jt_pnum;
-	public JButton add, delete, update, view; 
-	public Connection connect;
-	public Statement state;
-	public String std_id, std_name, std_depart, std_pnum;
-	
-	public StudentInfoSystem(){
-	
-		super("Student Information Book");		
-		init();
+    ResultSet rs = null;
+    
+     public static void main(String[] args) {
+    	 StudentInfoSystem sis = new  StudentInfoSystem();
+ 		 sis.MainMenu();       
+ 		 sis.setSize(700,400);
+		 sis.setVisible(true);
+     }
+     
+     private void MainMenu() {
+    	
+ 		init();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		DBconnect();
-	}
-	
-	private void DBconnect(){
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/student_info";
-			connect = DriverManager.getConnection(url,"root","0070");
-			state = connect.createStatement();
-		}catch(Exception e){
-			e.printStackTrace();	
-		}			
-	}
-	private void init(){
 		
-		JPanel top = new JPanel();
-		top.add(new JLabel("<STUDENT INFORMATION BOOK>"));
-		getContentPane().add("North", top);
-		
-		display = new JTextArea();
-		display.setEditable(true);
-		getContentPane().add("Center", new JScrollPane(display));
-		
-		JPanel left = new JPanel(new GridLayout(7,2));
-		left.setPreferredSize(new Dimension(200,400));
-		left.add(new JLabel("   학      번"));
-		left.add(jt_id=new JTextField(30));
-		left.add(new JLabel("   이      름"));
-		left.add(jt_name=new JTextField(30));
-		left.add(new JLabel("   학      과"));
-		left.add(jt_depart=new JTextField(30));
-		left.add(new JLabel("   핸드폰번호"));
-		left.add(jt_pnum=new JTextField(30));
-		getContentPane().add("West",left);		
-		
-		makeButton();
-	}
-	
-	
-	private void makeButton(){
-		JPanel button = new JPanel();
-		button.add(add=new JButton("ADD"));
-		add.addActionListener(this);
-		
-		button.add(delete=new JButton("DELETE"));
-		delete.addActionListener(this);
-		
-		button.add(update=new JButton("UPDATE"));
-		update.addActionListener(this);
-	
-		button.add(view=new JButton("VIEW"));
-		view.addActionListener(this);
-		
-		getContentPane().add("South", button);
-	}
-	
-	public void clear(){
-		jt_id.setText("");
-		jt_name.setText("");
-		jt_depart.setText("");
-		jt_pnum.setText("");
-	}
-	
-	public void actionPerformed(ActionEvent e) {
+ 	}
+     
+     private void init(){
+ 		
+ 		JPanel top = new JPanel();
+ 		top.add(new JLabel("<STUDENT INFORMATION BOOK>"));
+ 		getContentPane().add("North", top);
+ 		
+ 		display = new JTextArea();
+ 		display.setEditable(true);
+ 		getContentPane().add("Center", new JScrollPane(display));
+ 		
+ 		JPanel left = new JPanel(new GridLayout(7,2));
+ 		left.setPreferredSize(new Dimension(200,400));
+ 		left.add(new JLabel("   학      번"));
+ 		left.add(input_id=new JTextField(30));
+ 		left.add(new JLabel("   이      름"));
+ 		left.add(input_name=new JTextField(30));
+ 		left.add(new JLabel("   학      과"));
+ 		left.add(input_depart=new JTextField(30));
+ 		left.add(new JLabel("   핸드폰번호"));
+ 		left.add(input_pnum=new JTextField(30));
+ 		getContentPane().add("West",left);		
+ 		
+ 		makeButton();
+ 	}
+ 	
+ 	
+ 	private void makeButton(){
+ 		JPanel button = new JPanel();
+ 		button.add(add=new JButton("ADD"));
+ 		add.addActionListener(this);
+ 		
+ 		button.add(delete=new JButton("DELETE"));
+ 		delete.addActionListener(this);
+ 		
+ 		button.add(update=new JButton("UPDATE"));
+ 		update.addActionListener(this);
+ 	
+ 		button.add(view=new JButton("VIEW"));
+ 		view.addActionListener(this);
+ 		
+ 		getContentPane().add("South", button);
+ 	}
+ 	
+ 	public void actionPerformed(ActionEvent e) {
+ 		
+ 		StudentInfoDB db = new StudentInfoDB();
+		db.dataBase();
 		Component c = (Component)e.getSource();
 		
-		String s_id = jt_id.getText().trim();
-		String s_name = jt_name.getText().trim();
-		String s_depart = jt_depart.getText().trim();
-		String s_pnum = jt_pnum.getText().trim();
-		
-		boolean result;
-		
-		StudentInfoDB db = new StudentInfoDB(s_id, s_name, s_depart, s_pnum);
-		
-		if(c == add){
-			
-			result = db.info_add();
-						
-			if(result){
-				clear();
-				display.append(" =============================================================" +"\n");
-				display.append(" \t     학생 정보가 추가되었습니다.\n ");
-				display.append(" ============================================================="+"\n");	
-				}	
+		if(c==add){
+			db.add();
+			clear();
 		}
-		}
-	
-	public static void main(String[] args){
-		StudentInfoSystem s = new StudentInfoSystem();
-		s.setSize(600,400);
-		s.setVisible(true);
 	}
+ 	
+ 	public void clear(){
+		input_id.setText("");
+		input_name.setText("");
+		input_depart.setText("");
+		input_pnum.setText("");
+	}
+     
+	public static Connection makeConnection(){
+		String url = "jdbc:mysql://" + _host + ":" + _port + "/" + _database;
+        Connection con = null;        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, _user, _password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }        
+        return con;
+    }
+	
 }
